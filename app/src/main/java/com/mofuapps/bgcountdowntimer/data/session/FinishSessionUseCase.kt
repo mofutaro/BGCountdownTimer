e6b5357f1705987d9cbd97178f8b01e0c346d89b
mofutaro <jnakadev@gmail.com>
2023-05-26 22:48:06 +0900

    feat: FinishSessionUseCaseを実装

--- app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/FinishSessionUseCase.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/FinishSessionUseCase.kt
@@ -1,5 +1,21 @@
 package com.mofuapps.bgcountdowntimer.data.session
 
-class FinishSessionUseCase {
-    operator fun invoke() {}
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.Date
+
+class FinishSessionUseCase(
+    private val sessionRepository: SessionRepository
+) {
+    suspend operator fun invoke() = withContext(Dispatchers.IO) {
+        val currentSession: Session? = sessionRepository.find()
+        currentSession?.let {
+            val updatedSession = it.copy(
+                progressMillisAtResumed = it.durationMillis(),
+                resumedAt = Date(),
+                state = SessionState.FINISHED
+            )
+            sessionRepository.update(updatedSession)
+        }
+    }
 }
