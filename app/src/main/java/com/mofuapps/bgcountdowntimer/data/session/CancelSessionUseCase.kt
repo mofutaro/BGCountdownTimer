5c0b218a85e873ad921062a334e46ecf18b0d96d
mofutaro <jnakadev@gmail.com>
2023-05-26 22:18:20 +0900

    feat: CancelSessionUseCaseを実装

--- app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/CancelSessionUseCase.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/CancelSessionUseCase.kt
@@ -1,5 +1,15 @@
 package com.mofuapps.bgcountdowntimer.data.session
 
-class CancelSessionUseCase {
-    operator fun invoke() {}
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class CancelSessionUseCase(
+    private val sessionRepository: SessionRepository
+) {
+    suspend operator fun invoke() = withContext(Dispatchers.IO) {
+        val currentSession: Session? = sessionRepository.find()
+        currentSession?.let {
+            sessionRepository.delete(it)
+        }
+    }
 }
