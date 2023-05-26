23f4188e23da5b7d98253ce9f8642363b66c51f6
mofutaro <jnakadev@gmail.com>
2023-05-26 22:41:40 +0900

    feat: Sessionでフィールドを変換する関数を定義

--- app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/Session.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/Session.kt
@@ -1,12 +1,40 @@
 package com.mofuapps.bgcountdowntimer.data.session
 
 import java.util.Date
-
+import kotlin.math.max
 
 
 data class Session(
     val durationSec: Int,
-    val progressMillisAtResumed: Int,
-    val resumedAt: Date?,
+    val progressMillisAtResumed: Long,
+    val resumedAt: Date,
     val state: SessionState
-)
+) {
+    private fun isRunning(): Boolean {
+        return state == SessionState.RUNNING
+    }
+
+    private fun durationMillis(): Long {
+        return durationSec.toLong() * 1000L
+    }
+
+    private fun millisUntil(date: Date): Long {
+        return date.time - resumedAt.time
+    }
+
+    fun currentProgressMillis(): Long {
+        return if (isRunning())
+            progressMillisAtResumed + millisUntil(Date())
+        else
+            progressMillisAtResumed
+    }
+
+    fun remainingMillis(): Long {
+        return durationMillis() - currentProgressMillis()
+    }
+
+    fun progressPercent(): Double {
+        val percent: Double = currentProgressMillis() / durationMillis().toDouble()
+        return max(percent, 1.0) * 100L
+    }
+}
