20744edf8279e00c09ae1b02bd3d1201dc30b8ac
mofutaro <jnakadev@gmail.com>
2023-05-26 21:18:11 +0900

    feat: StartSessionUseCaseを実装

--- app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/StartSessionUseCase.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/StartSessionUseCase.kt
@@ -1,5 +1,18 @@
 package com.mofuapps.bgcountdowntimer.data.session
 
-class StartSessionUseCase {
-    operator fun invoke() {}
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class StartSessionUseCase(
+    private val sessionRepository: SessionRepository
+) {
+    suspend operator fun invoke(durationSec: Int) = withContext(Dispatchers.IO) {
+        val newSession = Session(
+            durationSec = durationSec,
+            progressMillisAtResumed = 0,
+            resumedAt = null,
+            state = SessionState.RUNNING
+        )
+        sessionRepository.insert(newSession)
+    }
 }
