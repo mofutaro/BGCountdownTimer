822b73dfb2b34e70693e031b9c725321f94ec557
mofutaro <jnakadev@gmail.com>
2023-05-27 16:38:47 +0900

    feat: MainActivityとMyAppを作成

---
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/ui/MyApp.kt
@@ -0,0 +1,11 @@
+package com.mofuapps.bgcountdowntimer.ui
+
+import androidx.compose.runtime.Composable
+import com.mofuapps.bgcountdowntimer.ui.theme.BGCountdownTimerTheme
+
+@Composable
+fun MyApp() {
+    BGCountdownTimerTheme {
+
+    }
+}
