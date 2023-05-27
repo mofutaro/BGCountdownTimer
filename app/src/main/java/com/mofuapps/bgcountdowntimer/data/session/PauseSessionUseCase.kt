1289adb484d6d05280aa188286482f4ed7ac707b
mofutaro <jnakadev@gmail.com>
2023-05-27 09:22:19 +0900

    feat: PauseSessionUseCaseを実装

--- app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/PauseSessionUseCase.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/PauseSessionUseCase.kt
@@ -1,5 +1,19 @@
 package com.mofuapps.bgcountdowntimer.data.session
 
-class PauseSessionUseCase {
-    operator fun invoke() {}
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class PauseSessionUseCase(
+    private val sessionRepository: SessionRepository
+) {
+    suspend operator fun invoke() = withContext(Dispatchers.IO) {
+        val currentSession = sessionRepository.find()
+        currentSession?.let {
+            val updatedSession = it.copy(
+                progressMillisAtResumed = it.currentProgressMillis(),
+                state = SessionState.PAUSED
+            )
+            sessionRepository.update(updatedSession)
+        }
+    }
 }
