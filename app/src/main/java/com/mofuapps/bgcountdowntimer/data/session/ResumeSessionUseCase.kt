520a4195132d2809813fea119031179dda155f50
mofutaro <jnakadev@gmail.com>
2023-05-27 09:54:29 +0900

    feat: ResumeSessionUseCaseを実装

--- app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/ResumeSessionUseCase.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/ResumeSessionUseCase.kt
@@ -1,5 +1,20 @@
 package com.mofuapps.bgcountdowntimer.data.session
 
-class ResumeSessionUseCase {
-    operator fun invoke() {}
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.Date
+
+class ResumeSessionUseCase(
+    private val sessionRepository: SessionRepository
+) {
+    suspend operator fun invoke() = withContext(Dispatchers.IO) {
+        val currentSession = sessionRepository.find()
+        currentSession?.let {
+            val updatedSession = currentSession.copy(
+                resumedAt = Date(),
+                state = SessionState.RUNNING
+            )
+            sessionRepository.update(updatedSession)
+        }
+    }
 }
