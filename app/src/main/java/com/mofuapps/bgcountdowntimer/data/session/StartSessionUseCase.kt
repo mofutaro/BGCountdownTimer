d62d359bff6b8136e2253145ccfee393b5b22337
mofutaro <jnakadev@gmail.com>
2023-05-27 10:04:34 +0900

    fix: Sessionを適切なパラメータで初期化

--- app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/StartSessionUseCase.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/StartSessionUseCase.kt
@@ -2,6 +2,7 @@ package com.mofuapps.bgcountdowntimer.data.session
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.util.Date
 
 class StartSessionUseCase(
     private val sessionRepository: SessionRepository
@@ -10,7 +11,7 @@ class StartSessionUseCase(
         val newSession = Session(
             durationSec = durationSec,
             progressMillisAtResumed = 0,
-            resumedAt = null,
+            resumedAt = Date(),
             state = SessionState.RUNNING
         )
         sessionRepository.insert(newSession)
