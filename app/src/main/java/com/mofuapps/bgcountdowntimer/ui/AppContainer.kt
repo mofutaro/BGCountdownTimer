70ada8db6ed82bbadffd4dac550bad96a74a429e
mofutaro <jnakadev@gmail.com>
2023-05-30 00:32:08 +0900

    feat: finishSessionUseCaseを宣言

--- app/src/main/java/com/mofuapps/bgcountdowntimer/ui/AppContainer.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/ui/AppContainer.kt
@@ -4,15 +4,17 @@ import android.app.Application
 import com.mofuapps.bgcountdowntimer.data.getDatabase
 import com.mofuapps.bgcountdowntimer.data.session.RealSessionRepository
 import com.mofuapps.bgcountdowntimer.domain.session.CancelSessionUseCase
+import com.mofuapps.bgcountdowntimer.domain.session.FinishSessionUseCase
 import com.mofuapps.bgcountdowntimer.domain.session.PauseSessionUseCase
 import com.mofuapps.bgcountdowntimer.domain.session.ResumeSessionUseCase
 import com.mofuapps.bgcountdowntimer.domain.session.StartSessionUseCase
 
 class AppContainer(application: Application) {
-    val database = getDatabase(application)
+    private val database = getDatabase(application)
     val sessionRepository = RealSessionRepository(database.sessionDao)
     val startSessionUseCase = StartSessionUseCase(sessionRepository)
     val pauseSessionUseCase = PauseSessionUseCase(sessionRepository)
     val resumeSessionUseCase = ResumeSessionUseCase(sessionRepository)
     val cancelSessionUseCase = CancelSessionUseCase(sessionRepository)
+    val finishSessionUseCase = FinishSessionUseCase(sessionRepository)
 }
