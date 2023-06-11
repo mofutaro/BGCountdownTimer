ceb95ae4a11cf2879221bc460d3c2a1ab58d91bd
mofutaro <jnakadev@gmail.com>
2023-06-11 19:04:42 +0900

    feat: IoDispatcherをRealSessionRepositoryに挿入する

--- app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/RealSessionRepository.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/data/session/RealSessionRepository.kt
@@ -1,8 +1,9 @@
 package com.mofuapps.bgcountdowntimer.data.session
 
+import com.mofuapps.bgcountdowntimer.di.IoDispatcher
 import com.mofuapps.bgcountdowntimer.domain.session.Session
 import com.mofuapps.bgcountdowntimer.domain.session.SessionRepository
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.transform
@@ -11,24 +12,27 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class RealSessionRepository @Inject constructor(private val sessionDao: SessionDao): SessionRepository {
+class RealSessionRepository @Inject constructor(
+    private val sessionDao: SessionDao,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
+    ): SessionRepository {
     override val flow: Flow<Session?> = sessionDao.findFlow().transform {
         emit(it?.asDomainModel())
-    }.flowOn(Dispatchers.IO)
+    }.flowOn(dispatcher)
 
-    override suspend fun find(): Session? = withContext(Dispatchers.IO) {
+    override suspend fun find(): Session? = withContext(dispatcher) {
         sessionDao.find()?.asDomainModel()
     }
 
-    override suspend fun insert(session: Session) = withContext(Dispatchers.IO) {
+    override suspend fun insert(session: Session) = withContext(dispatcher) {
         sessionDao.insert(session.asDatabaseModel())
     }
 
-    override suspend fun update(session: Session) = withContext(Dispatchers.IO) {
+    override suspend fun update(session: Session) = withContext(dispatcher) {
         sessionDao.update(session.asDatabaseModel())
     }
 
-    override suspend fun delete(session: Session) = withContext(Dispatchers.IO) {
+    override suspend fun delete(session: Session) = withContext(dispatcher) {
         sessionDao.delete(session.asDatabaseModel())
     }
 }
