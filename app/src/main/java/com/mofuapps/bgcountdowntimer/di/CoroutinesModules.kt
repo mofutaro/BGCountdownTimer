fa1ceb627022b5f72dd932e4fa169143da17c7b2
mofutaro <jnakadev@gmail.com>
2023-06-11 21:40:12 +0900

    feat: Applicationプロセスに依存するCoroutineScopeを作成

--- app/src/main/java/com/mofuapps/bgcountdowntimer/di/CoroutinesModules.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/di/CoroutinesModules.kt
@@ -5,7 +5,10 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
 
 
 @Module
@@ -15,4 +18,15 @@ object CoroutinesModule {
     @Provides
     @IoDispatcher
     fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @DefaultDispatcher
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @Singleton
+    @ApplicationScope
+    fun providesCoroutineScope(
+        @DefaultDispatcher dispatcher: CoroutineDispatcher
+    ): CoroutineScope = CoroutineScope(SupervisorJob() + dispatcher)
 }
--- app/src/main/java/com/mofuapps/bgcountdowntimer/di/Dispatchers.kt
+++ app/src/main/java/com/mofuapps/bgcountdowntimer/di/Dispatchers.kt
@@ -2,6 +2,14 @@ package com.mofuapps.bgcountdowntimer.di
 
 import javax.inject.Qualifier
 
-@Qualifier
 @Retention(AnnotationRetention.RUNTIME)
+@Qualifier
 annotation class IoDispatcher
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
